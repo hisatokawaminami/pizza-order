5953d956ac071efaac8894eaac550fbc52e39f0f
Hisato Kawaminami <hisatokawaminami@gmail.com>
2018-06-15 15:58:50 -0700

    update business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,25 @@
 //business logic
-function Pizza (name, size, toppings){
+function Pizza (name, size, toppings, total){
   this.name = name;
   this.size = size;
   this.toppings = []
-
+  // this.total = total
 }
 
+// Pizza.prototype.total = function(total){
+//   var toppingPrice = this.topping.length * 1.5;
+//   if (this.size === "large") {
+//     total = 18 + toppingPrice;
+//   } else if (this.size === "medium"){
+//     total = 15 + toppingPrice;
+//   } else {
+//     total = 10 + toppingPrice;
+//   }
+//   // this.toppings.forEach(function(){
+//   //   total += 1.5
+//   // });
+//     return this.total
+//   };
 
 
 //userinterface
@@ -14,18 +28,35 @@ $(function(){
     event.preventDefault();
   var username = $("input#name").val()
   var selectedSize = $("input:radio[name=size]:checked").val()
+
   var newPizza = new Pizza(username, selectedSize);
 
   $("input:checkbox[name='topping']:checked").each(function(){
       newPizza.toppings.push($(this).val());
     });
 
+    function sizePrice(){
+      total = 0
+
+        if (selectedSize === "Large") {
+          total = 20
+        } else if (selectedSize === "Medium"){
+          total = 15
+        } else {
+          total = 10
+
+    } return total
+    }
+    var basePrice = sizePrice()
+    var totalPrice = basePrice + newPizza.toppings.length*1.5
+
+    // newPizza.total(total)
       $(".name").text(newPizza.name);
       $(".size").text(newPizza.size);
       $(".toppings").text(newPizza.toppings);
-      $("price").text();
-      // $(".output").show();
-      console.log(this.size)
+      $(".price").text("$"+totalPrice.toFixed(2));
+      $(".output").show();
+      console.log(newPizza.total)
     });
 
   });
